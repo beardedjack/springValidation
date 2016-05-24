220af9b83014a90c433bee60ccd53a94c1006a6e
kondrashov <kondrashov@KONDRASHOV.main.dom>
2016-05-24 17:18:42 +0400

    init project

---
+++ src/java/com/evgen/mvc/entity/User.java
@@ -0,0 +1,9 @@
+package com.evgen.mvc.entity;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import javax.validation.constraints.Past;
+import java.util.Date;
+
+public class User {
+    
+}
